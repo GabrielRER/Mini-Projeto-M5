3826260c4423f334c633eea7e5fe9c37894a3238
GabrielRER <gabrielramonevangelistaramos@gmail.com>
2024-09-23 00:12:24 -0300

    add search bar

--- miniProjetoM4/src/pages/Home/index.jsx
+++ miniProjetoM4/src/pages/Home/index.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React from "react";
 import ListButton from "./components/ListButton/index.jsx";
 import style from "./style.module.css";
 import addIcon from "../../assets/add.png";
-import trashIcon from "../../assets/trashicon.png"
-import pencilIcon from "../../assets/pencil.png"
+import trashIcon from "../../assets/trashicon.png";
+import pencilIcon from "../../assets/pencil.png";
+import loupeIcon from "../../assets/loupe.png";
 
 function Home() {
 
 
   return (
+    <div className={style.container}>
+
     <div className={style.buttonContainer}> 
+
     <ul className={style.list}>
         <li className={style.btnList}><ListButton image={addIcon} alt={"addIcon"}/></li>
         <li className={style.btnList}><ListButton image={trashIcon} alt={"trashIcon"}/></li>
         <li className={style.btnList}><ListButton image={pencilIcon} alt={"pencilIcon"}/></li>
     </ul>
+
+    </div>
+
+    <div className={style.searchBox}>
+
+        <h3>Pesquise uma tarefa:</h3>
+        <input className={style.inputSearchBox}></input>
+        
+        
+    </div>
+
+    <ul className={style.searchButton}>
+        <li><ListButton image={loupeIcon} alt={"loupeIcon"}/></li>
+    </ul>
+
     </div>
   )
 }
